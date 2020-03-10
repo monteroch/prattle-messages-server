5ce1d2d59226736b00c327c579997a957838f132
fredomontero <christianmontero08@gmail.com>
2020-03-10 11:35:18 -0700

    Remove deprecated code

--- app.js
+++ app.js
@@ -11,17 +11,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    if(req.method === 'OPTIONS'){
-        return res.sendStatus(200);
-    }
-    next();
-});
-
 app.use('/graphql', graphqlHttp({
     schema: graphQLSchema,
     graphiql: true
